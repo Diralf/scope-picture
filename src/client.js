0d47d1778cbbb77176405080d822915e030c8b7c
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2022-05-25 15:57:47 +0300

    added console.log to be sure

--- src/client.js
+++ src/client.js
@@ -1,5 +1,8 @@
 TrelloPowerUp.initialize({
-    'card-buttons': (t, options) => [{
-        text: 'Hey, test button',
-    }],
+    'card-buttons': (t, options) => {
+        console.log('Power-up is working', {t, options});
+        return [{
+            text: 'Hey, test button',
+        }];
+    },
 });
