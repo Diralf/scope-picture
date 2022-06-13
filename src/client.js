07361b6d31f95c3c75e020044c9ad06273580550
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2022-06-13 16:49:19 +0300

    added sort of keys

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 TrelloPowerUp.initialize({
     'card-buttons': (t, options) => {
         console.log('Power-up is working', {t, options});
-        console.log(Object.keys(t).map((key) => `    ${key}: any;`).join('\n'));
+        console.log(Object.keys(t).sort().map((key) => `    ${key}: any;`).join('\n'));
         return [{
             text: 'Hey, test button',
         }];
