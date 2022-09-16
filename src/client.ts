a03318e5dfa31d0d67d0d8b7b11c66b71b9a49b6
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2022-09-16 14:16:37 +0300

    added board to getset results

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,10 @@
 window.TrelloPowerUp.initialize({
     'card-buttons': (t, options) => {
-        const card = t.card('all');
-        console.log('Power-up is working', {t, options, card});
+        const getset = {
+            card: t.card('all'),
+            board: t.board('all'),
+        };
+        console.log('Power-up is working', {t, options, ...getset});
         return [{
             text: 'Hey, test button',
         }];
